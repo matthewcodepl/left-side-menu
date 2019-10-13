15b73bbc81ed657e2d3fb8a513e2837af7337188
matthewcodepl <testy12221@gmail.com>
2019-10-13 11:55:46 +0200

    end navigation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const menu = document.getElementById('toggle');
 const toggleMenu = document.getElementById('sub');
-
+const bar = document.getElementById('burger');
+const topMenu = document.getElementById('all');
 const handleClick = () => {
 
     toggleMenu.classList.toggle('add');
 
 }
 
+const handleBarClick = () => {
+    topMenu.classList.toggle('right');
+    bar.classList.toggle('slow');
+}
 
+
+bar.addEventListener('click', handleBarClick)
 menu.addEventListener('click', handleClick)
